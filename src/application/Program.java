bfff3c2a6a80e1a8d2f74a79d75881c983d54318
RafaelBonotto <rafareisaba@gmail.com>
2020-09-02 09:58:06 -0300

    Classe Vendedor, Interface, ClasseDao e Factory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
-import model.entities.Departamento;
+import model.dao.DaoFactory;
+import model.dao.VendedorDao;
+import model.entities.Vendedor;
 
 public class Program {
 
 	public static void main(String[] args) {
+					
+		VendedorDao vendedorDao = DaoFactory.createVendedorDao();
 		
-		Departamento obj = new Departamento (1, "livros");
-		System.out.println(obj);
+		Vendedor vendedor = vendedorDao.buscaPorId(3);
+		
+		System.out.println(vendedor);
 	}
 
 }
---
+++ src/model/dao/VendedorDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Vendedor;
+
+public interface VendedorDao {
+	
+	void inserir (Vendedor obj);	
+	void atualizar(Vendedor obj);
+	void excluirPorId(Integer id);
+	Vendedor buscaPorId(Integer id);
+	List<Vendedor> buscarTodos();
+}
