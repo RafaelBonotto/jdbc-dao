78720f72a8e60888cb78859bf30788bb70164c32
RafaelBonotto <rafareisaba@gmail.com>
2020-09-02 22:40:24 -0300

    Buscar, Inserir, Atualizar

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
+import java.util.Date;
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.VendedorDao;
+import model.entities.Departamento;
 import model.entities.Vendedor;
 
 public class Program {
@@ -10,11 +14,36 @@ public class Program {
 					
 		VendedorDao vendedorDao = DaoFactory.createVendedorDao();
 		
-		System.out.println("=== Test 1: vendedor buscaPorId ===");
+		System.out.println("=== Test 1: vendedor buscarPorId ===");
 		
 		Vendedor vendedor = vendedorDao.buscaPorId(3);
 		
 		System.out.println(vendedor);
-	}
+		
+		System.out.println("\n=== Test 2: vendedor buscarPorDepartamento ===");
+		Departamento departamento = new Departamento(2, null);
+		List<Vendedor> list = vendedorDao.buscarPorDepartamento(departamento);
+		for (Vendedor obj : list) {
+			System.out.println(obj);
+		}
+			
+		System.out.println("\n=== Test 3: vendedor buscarTodos ===");
+		list = vendedorDao.buscarTodos();
+		   for (Vendedor obj : list) {
+				System.out.println(obj);
+		}
+		   
+		System.out.println("\n=== Test 4: vendedor anserir  ===");
+		Vendedor newVendedor = new Vendedor(null, "Greg", "greg@gmail.com", new Date(), 4000.0, departamento);
+		vendedorDao.inserir(newVendedor);
+		System.out.println("Inserido! Novo Id: " + newVendedor.getId());
+		
+		System.out.println("\n=== Test 5: vendedor atualizar  ===");
+		vendedor = vendedorDao.buscaPorId(1);
+		vendedor.setNome("Marta Waine");
+		vendedorDao.atualizar(vendedor);
+		System.out.println("Atualização Completada!");
 
-}
+	   
+     }
+}	
--- src/model/dao/impl/VendedorDaoJDBC.java
+++ src/model/dao/impl/VendedorDaoJDBC.java
@@ -4,7 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -22,14 +26,68 @@ public class VendedorDaoJDBC implements VendedorDao {
 
 	@Override
 	public void inserir(Vendedor obj) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					  "INSERT INTO seller "
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES "
+					+ "( ?, ? , ? , ? , ?)",
+					Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, obj.getNome());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getDataNascimento().getTime()));
+			st.setDouble(4, obj.getSalarioBase());
+			st.setInt(5, obj.getDepartamento().getId());
+			
+			int linhasAfetadas = st.executeUpdate();
+			
+			if(linhasAfetadas > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if(rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+				DB.closeResultSet(rs);
+			}
+			else {
+				throw new DbException("ERRO Inesperado! Nenhuma linha foi afetada!");
+			}
+									
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
 	public void atualizar(Vendedor obj) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					  "UPDATE seller "
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+					+ "WHERE Id = ?");
+			
+			st.setString(1, obj.getNome());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getDataNascimento().getTime()));
+			st.setDouble(4, obj.getSalarioBase());
+			st.setInt(5, obj.getDepartamento().getId());
+			st.setInt(6, obj.getId());
+			
+			st.executeUpdate();															
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}		
 	}
 
 	@Override
@@ -87,8 +145,85 @@ public class VendedorDaoJDBC implements VendedorDao {
 
 	@Override
 	public List<Vendedor> buscarTodos() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+				  "SELECT seller.*,department.Name as DepName "
+				+ "FROM seller INNER JOIN department "	
+				+ "ON seller.DepartmentId = department.Id "
+				+ "ORDER BY Name");	
+			
+			rs = st.executeQuery();
+			
+			List<Vendedor> list = new ArrayList<>();
+			
+			Map<Integer, Departamento> map =new HashMap<>();
+			
+			while(rs.next()) {
+				
+				Departamento dep = map.get(rs.getInt("DepartmentId"));
+				
+				if(dep == null) {
+					dep = instanciarDepartamento(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Vendedor obj = instanciarVendedor(rs, dep);
+				list.add(obj);
+			}
+			return list;
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
+
+	@Override
+	public List<Vendedor> buscarPorDepartamento(Departamento departamento) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+				  "SELECT seller.*,department.Name as DepName "
+				+ "FROM seller INNER JOIN department "	
+				+ "ON seller.DepartmentId = department.Id "
+				+ "WHERE DepartmentId = ? "
+				+ "ORDER BY Name");
+			
+			st.setInt(1, departamento.getId());
+			
+			rs = st.executeQuery();
+			
+			List<Vendedor> list = new ArrayList<>();
+			
+			Map<Integer, Departamento> map =new HashMap<>();
+			
+			while(rs.next()) {
+				
+				Departamento dep = map.get(rs.getInt("DepartmentId"));
+				
+				if(dep == null) {
+					dep = instanciarDepartamento(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Vendedor obj = instanciarVendedor(rs, dep);
+				list.add(obj);
+			}
+			return list;
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 	
 }
